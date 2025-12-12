4e5309a0efac29d4182f273fb64db3a3553a6eb3
mkdodos <mkdodos@gmail.com>
2025-12-12 10:34:07 +0800

    aaa

--- src/pages/Table/components/BasicTable.js
+++ src/pages/Table/components/BasicTable.js
@@ -19,35 +19,17 @@ import "./style.css";
 // import { data } from "react-router-dom";
 
 export default function index() {
-  
-  const data2 = [
-    {
-      name: "馬克",
-    },
-    {
-      name: "宜君",
-    },
-    {
-      name: "愷軒",
-    },
-    {
-      name: "欣妤",
-    },
-    {
-      name: "子晉",
-    },
-  ];
   return (
     // basic='very' 無外框
-    <Table basic='very' className="basic" unstackable striped padded>
+    <Table basic="very" className="basic" unstackable striped padded>
       <TableHeader>
         <TableRow>
           <TableHeaderCell>日期</TableHeaderCell>
-          <TableHeaderCell>地點</TableHeaderCell>         
+          <TableHeaderCell>地點</TableHeaderCell>
           <TableHeaderCell>人員</TableHeaderCell>
           <TableHeaderCell>
             <Button primary icon>
-              <Icon  name="plus" />
+              <Icon name="plus" />
             </Button>
           </TableHeaderCell>
         </TableRow>
@@ -58,7 +40,12 @@ export default function index() {
           <TableCell>11-15</TableCell>
           <TableCell>瑞復</TableCell>
           <TableCell></TableCell>
-          <TableCell></TableCell>
+          <TableCell>
+            {" "}
+            <Button icon onClick={() => handleEdit(row, index)}>
+              <Icon name="pencil" />
+            </Button>
+          </TableCell>
         </TableRow>
         <TableRow className="abc" top aligned>
           <TableCell>11-08</TableCell>
--- src/pages/Table/components/TableForm.js
+++ src/pages/Table/components/TableForm.js
@@ -16,7 +16,7 @@ import {
 } from "semantic-ui-react";
 // import People from "../Label";
 
-import Dropdown from "../../Dropdown"
+import Dropdown from "../../Dropdown";
 
 import "./style.css";
 // import { data } from "react-router-dom";
@@ -24,60 +24,42 @@ import "./style.css";
 export default function index() {
   return (
     // basic='very' 無外框
-    <Table  basic="very" className="basic" unstackable striped>
-      {/* <TableHeader>
-        <TableRow>
-          <TableHeaderCell width={3}>日期</TableHeaderCell>
-          <TableHeaderCell>地點</TableHeaderCell>
-          <TableHeaderCell>人員</TableHeaderCell>
-          <TableHeaderCell>人員</TableHeaderCell>
-          <TableHeaderCell>人員</TableHeaderCell>
-          <TableHeaderCell>人員</TableHeaderCell>
-          <TableHeaderCell>人員</TableHeaderCell>
-          <TableHeaderCell>
-            <Button primary icon>
-              <Icon name="plus" />
-            </Button>
-          </TableHeaderCell>
-        </TableRow>
-      </TableHeader> */}
-
+    <Table basic="very" className="basic" unstackable striped>
       <TableBody>
         <TableRow>
           <TableCell>客戶</TableCell>
           <TableCell>
-            <Dropdown/>
+            <Dropdown />
           </TableCell>
           <TableCell>案號</TableCell>
-          <TableCell >
-            <Input  />
+          <TableCell>
+            <Input />
           </TableCell>
           <TableCell>品名</TableCell>
           <TableCell>
-            <Input  />
-          </TableCell>
-          {/* <TableCell>加工說明</TableCell>
-          <TableCell>
             <Input />
-          </TableCell> */}
+          </TableCell>
         </TableRow>
         <TableRow>
-          <TableCell >尺寸1</TableCell>
+          <TableCell>尺寸1</TableCell>
           <TableCell>
             <Input />
           </TableCell>
-
           <TableCell>尺寸2</TableCell>
           <TableCell>
-            <Input  />
+            <Input />
           </TableCell>
           <TableCell>尺寸3</TableCell>
           <TableCell>
-            <Input  />
+            <Input />
           </TableCell>
-          {/* <TableCell>按鈕</TableCell>
-          <TableCell></TableCell> */}
         </TableRow>
+        <TableRow>
+            <TableCell>說明</TableCell>
+            <TableCell colSpan="5">
+              <Input fluid />
+            </TableCell>
+          </TableRow>
       </TableBody>
     </Table>
   );
